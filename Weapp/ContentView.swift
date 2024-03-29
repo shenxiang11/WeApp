7bbf47a1bb2e3478d11ac5448070421ca65c92e8
Shen <863461783@qq.com>
2024-03-29 17:37:01 +0800

    Initial Commit

---
+++ Weapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weapp
+//
+//  Created by 香饽饽zizizi on 2024/3/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weapp/WeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeappApp.swift
+//  Weapp
+//
+//  Created by 香饽饽zizizi on 2024/3/29.
+//
+
+import SwiftUI
+
+@main
+struct WeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
